135392d02ad725290f131bc0b2ca206b92d85e7c
Philipp <philipp@f1ndus.de>
2022-12-02 19:22:42 +0100

    day2

--- src/main/scala/day2.scala
+++ src/main/scala/day2.scala
@@ -5,104 +5,38 @@ case class OutCome(outcome: String, points: Int)
 
 object day2 extends App {
 
-  object Result extends Enumeration {
-    val Win  = Value(6)
-    val Draw = Value(3)
-    val Loss = Value(0)
-  }
-
-  val ROCK = "Rock"
-  val PAPER = "Paper"
-  val SCISSOR = "Scissor"
-
-  val rockResponse = Response(ROCK, 1)
-  val paperResponse = Response(PAPER, 2)
-  val scissorsResponse = Response(SCISSOR, 3)
-
-  def getYourTurn(input: String) = {
+  def getNumber(input: String) = {
     input match {
-      case "X" => rockResponse
-      case "Y" => paperResponse
-      case "Z" => scissorsResponse
+      case "A" | "X" => 1    //Rock //Lose
+      case "B" | "Y" => 2    //Paper //Draw
+      case "C" | "Z" => 3   //Scissors //Win
     }
   }
 
-  def calcResult(enemyTurn: String) = {
-    enemyTurn match {
-      case ROCK =>
-      case PAPER =>
-      case SCISSOR =>
 
+  def winner(p1: Int, p2: Int): Int = {
+    val result = Math.floorMod(p1 - p2, 3)
+    if (result == 2) {
+      6
+    } else if (result == 0) {
+      3
+    } else {
+      0
     }
-  }
-
-  def getEnemyTurn(input: String) = {
-    input match {
-      case "A" => ROCK
-      case "B" => PAPER
-      case "C" => SCISSOR
-    }
-  }
-
-  def getResult(input: Round) = {
-    val et = input.enemyTurn
-    val yt = input.yourTurn.whatType
-    if (et.equals(yt))                                 Result.Draw
-    else if (yt.equals(ROCK)    && et.equals(PAPER))   Result.Loss
-    else if (yt.equals(ROCK)    && et.equals(SCISSOR)) Result.Win
-    else if (yt.equals(PAPER)   && et.equals(SCISSOR)) Result.Loss
-    else if (yt.equals(PAPER)   && et.equals(ROCK))    Result.Win
-    else if (yt.equals(SCISSOR) && et.equals(PAPER))   Result.Win
-    else if (yt.equals(SCISSOR) && et.equals(ROCK))    Result.Loss
-    else Result.Loss
   }
 
   io.load("day2") { lines =>
-
-     val rounds = lines.map(line => {
+     val rounds = lines.foldLeft(0)((prev, line) => {
        val split = line.split(" ")
-       Round(getEnemyTurn(split.head), getYourTurn(split.last))
+       prev + winner(getNumber(split.head), getNumber(split.last)) +  getNumber(split.last)
      })
 
-    val e = rounds.map(round => (getResult(round), round.yourTurn.count))
-    val results = e.map(ee => ee._2 + ee._1.id)
-    println(results.sum)
-
-  }
-
-  def getNeededOutCome(input: String) = {
-    input match {
-      case "X" => Result.Loss
-      case "Y" => Result.Draw
-      case "Z" => Result.Win
-    }
-  }
-
-  def getPoints(enemyTurn: String, neededOutCome: day2.Result.Value) = {
-    enemyTurn match {
-      case ROCK if neededOutCome == day2.Result.Loss => scissorsResponse
-      case ROCK if neededOutCome == day2.Result.Draw => rockResponse
-      case ROCK if neededOutCome == day2.Result.Win => paperResponse
-      case PAPER if neededOutCome == day2.Result.Loss => rockResponse
-      case PAPER if neededOutCome == day2.Result.Draw => paperResponse
-      case PAPER if neededOutCome == day2.Result.Win => scissorsResponse
-      case SCISSOR if neededOutCome == day2.Result.Loss => paperResponse
-      case SCISSOR if neededOutCome == day2.Result.Win => rockResponse
-      case SCISSOR if neededOutCome == day2.Result.Draw => scissorsResponse
-    }
+    println(rounds)
   }
 
   io.load("day2b") { lines =>
+   //todo rewrite when i get it
 
-    val out = lines.foldLeft(0)((prev,line) => {
-      val split = line.split(" ")
-      val enemyTurn = getEnemyTurn(split.head)
-      val neededOutcome = getNeededOutCome(split.last)
-      val points = getPoints(enemyTurn, neededOutcome)
-      prev + points.count + neededOutcome.id
-    })
-
-    println(out)
   }
 
 }
