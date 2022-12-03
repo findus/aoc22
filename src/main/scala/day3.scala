0153baa42371504827955b7255a62eba5a5e4faa
Philipp <philipp@f1ndus.de>
2022-12-03 11:08:24 +0100

    day3b

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -1,12 +1,16 @@
 object day3 extends App {
 
-  def getAllCharsPresentInBoth(set: List[Set[Char]]): Set[Char] = { set.head.intersect(set.last) }
-
+  def getAllCharsPresentInAll(set: List[Set[Char]]): Set[Char] = { set.foldLeft(set.head)((prev,next) => prev.intersect(next) ) }
   def getPosition(character: Char) = { character.toInt - (if (character.isUpper) { 38 } else { 96 }) }
 
   io.load("day3") { lines =>
     val items = lines.map(line => line.grouped(line.length / 2).map(_.sorted.distinct.toSet).toList)
-    println(items.flatMap(set => getAllCharsPresentInBoth(set).map(getPosition)).sum)
+    println(items.flatMap(set => getAllCharsPresentInAll(set).map(getPosition)).sum)
+  }
+
+  io.load("day3") { lines =>
+    val items = lines.map(_.sorted.distinct.toSet).sliding(3,3).toList
+    println(items.flatMap(setOfThree => getAllCharsPresentInAll(setOfThree)).map(getPosition).sum)
   }
 
 }
