d041d567b788106b9f0a3ba19b21e804ad3b4012
Philipp <philipp@f1ndus.de>
2022-12-04 19:52:41 +0100

    day4b

--- src/main/scala/day4.scala
+++ src/main/scala/day4.scala
@@ -6,7 +6,8 @@ object day4 extends App {
   }
 
   io.load("day4") { lines =>
-
+    val entries = lines.map(line => (line.split(",").head.split("-").map(Integer.parseInt),line.split(",").last.split("-").map(Integer.parseInt)))
+    println(entries.map(entry => (Range.inclusive(entry._1(0), entry._1(1)), Range.inclusive(entry._2(0), entry._2(1)))).count(entry => entry._1.intersect(entry._2).nonEmpty))
   }
 
 }
