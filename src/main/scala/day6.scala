271e98f3f61cddd31d18d2db73b8fe56827cfaed
Philipp <philipp@f1ndus.de>
2022-12-06 06:18:09 +0100

    day6

---
+++ src/main/scala/day6.scala
@@ -0,0 +1,18 @@
+import day5.run
+
+object day6 extends App {
+
+  def hasOnlyDifferentChars(list: List[Char]) = {
+    list.sorted.distinct.length == list.length
+  }
+
+  io.load("day6") { lines =>
+    val haval_gib_mir_einfach_ein_köftespieß = lines.map(line => line.zipWithIndex.sliding(4,1).filter(entry => hasOnlyDifferentChars(entry.map(_._1).toList)).toList.head.last._2 + 1)
+    println(haval_gib_mir_einfach_ein_köftespieß.head)
+  }
+
+  io.load("day6") { lines =>
+    val haval_gib_mir_einfach_ein_köftespieß = lines.map(line => line.zipWithIndex.sliding(14,1).filter(entry => hasOnlyDifferentChars(entry.map(_._1).toList)).toList.head.last._2 + 1)
+    println(haval_gib_mir_einfach_ein_köftespieß.head)
+  }
+}
