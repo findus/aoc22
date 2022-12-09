b9ad45283cdb98fc06133cbe0242d5960f7e455b
Philipp <philipp@f1ndus.de>
2022-12-09 13:42:48 +0100

    day9b

--- src/main/scala/day9.scala
+++ src/main/scala/day9.scala
@@ -30,54 +30,77 @@ object day9 extends App {
 
   def addTuple(a: (Int,Int), b: (Int,Int)) = (a._1 + b._1, a._2 + b._2)
 
-  def calcNewTPos(coordT: (Int, Int), newHpos: (Int, Int), preHpos: (Int,Int)) = {
-    val newDistance = (newHpos._1 - coordT._1,newHpos._2 - coordT._2)
+  def calcNewTPos(coordT: (Int, Int), newHpos: (Int, Int)) = {
+    val newDistance = (newHpos._1 - coordT._1, newHpos._2 - coordT._2)
     (newDistance._1.abs, newDistance._2.abs) match {
       case (1,0) | (0,1) | (1,1) => coordT
       case (2,0) if newDistance._1 < 0 => (coordT._1 - 1, coordT._2)
       case (2,0) if newDistance._1 > 0 => (coordT._1 + 1, coordT._2)
-      case (0,2) if newDistance._1 < 0 => (coordT._1                 , coordT._2 - 1)
-      case (0,2) if newDistance._1 > 0 => (coordT._1                 , coordT._2 + 1)
+      case (0,2) if newDistance._2 < 0 => (coordT._1                 , coordT._2 - 1)
+      case (0,2) if newDistance._2 > 0 => (coordT._1                 , coordT._2 + 1)
       case (0,0) => newHpos
-      case diagonal => preHpos
+      case _ => newDistance match {
+        case ( 1, 2) | ( 2, 1) | ( 2, 2)  => addTuple(coordT,( 1, 1))
+        case ( 1,-2) | ( 2,-1) | ( 2,-2)  => addTuple(coordT,( 1,-1))
+        case (-1, 2) | (-2, 1) | (-2, 2)  => addTuple(coordT,(-1, 1))
+        case (-1,-2) | (-2,-1) | (-2,-2)  => addTuple(coordT,(-1,-1))
+      }
     }
   }
 
   type Coord = (Int,Int)
   case class Action(position: ActionType)
-  case class State(coordH: Coord, coordT: Coord, vivistedT: List[Coord])
+  case class State(name: String, coord: Coord, visited: List[Coord])
 
-  def printState(newState: State) = {
-    (0 to newState.coordT._2.max(newState.coordH._2)).reverse.foreach { y =>
-      (0 to  newState.coordT._1.max(newState.coordH._1)).foreach { x =>
-        if (newState.coordH.equals((x,y))) print("H")
-        else if (newState.coordT.equals((x,y))) print("T")
-        else print(".")
+  def printState(states: List[State]) = {
+    (0 to 50).reverse.foreach { y =>
+      (0 to 50).foreach { x =>
+        val entry = states.find { entry => entry.coord.equals((x, y)) }
+        if (entry.isDefined) { print(entry.get.name) }  else { print(".")}
       }
       println("")
     }
     println("")
   }
 
-  io.load("day9") { lines =>
+  private def simulate(actions: List[Action], start: List[State]) = {
+    val state = actions.foldLeft(start)((prev1, action) => {
+      val newHpos =   prev1.head.copy(coord = addTuple(prev1.head.coord, ActionType.value(action.position)))
+      val newStates = prev1.drop(1).foldLeft(List(newHpos))((prev,input) => {
+        val a = prev.last
+        val b = input
+
+        val newTpos = calcNewTPos(b.coord, a.coord)
+        prev.appended(State(b.name, newTpos, b.visited.appended(newTpos)))
+      })
+
+     // printState(newStates)
+      newStates
+    })
+    val visits = state.last.visited.distinct
+    println(visits.size)
+  }
+
+  private def parseActions(lines: List[String]) = {
     val actions = lines.flatMap(line => {
       val extrapolate = 0 until Integer.parseInt(line.split(" ")(1))
       extrapolate.map(_ => Action(ActionType.from(line.split(" ")(0))))
     })
-
-    val initialState = State((0,0), (0,0), List.empty[Coord])
-    val state = actions.foldLeft(initialState)((prev, action) => {
-      val newHpos = addTuple(prev.coordH,ActionType.value(action.position))
-      val newTpos = calcNewTPos(prev.coordT, newHpos, prev.coordH)
-      val newState = State(newHpos, newTpos, prev.vivistedT.appended(newTpos))
-      printState(newState)
-      newState
-    })
-    val visits = state.vivistedT.distinct
-    println(visits.size)
+    actions
   }
 
   io.load("day9") { lines =>
+    val actions: List[day9.Action] = parseActions(lines)
+    val initialStateH = State("H", (0,0), List.empty[Coord])
+    val initialStateT = State("T", (0,0), List.empty[Coord])
+    val start = List(initialStateH, initialStateT)
+    simulate(actions, start)
+  }
 
+  io.load("day9") { lines =>
+    val actions: List[day9.Action] = parseActions(lines)
+    val initialStateH = State("H", (0,0), List.empty[Coord])
+    val rope = (1 to 9).map(nr => State(nr.toString, (0,0), List.empty[Coord])).toList.prepended(initialStateH)
+    simulate(actions, rope)
   }
 }
