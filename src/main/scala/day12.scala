0112ef268229ccf500ebe76790fc19ac4d179fcd
Philipp <philipp@f1ndus.de>
2022-12-12 19:18:58 +0100

    day12b

--- src/main/scala/day12.scala
+++ src/main/scala/day12.scala
@@ -2,13 +2,16 @@ import scala.annotation.tailrec
 
 object day12 extends App {
 
-  private def getGraph(entries: Map[(Int,Int), Int]): Map[(Int, Int), Map[(Int, Int), Int]] = {
+  implicit var entries: Map[(Int, Int), Int] = null
+  var triggered = false;
+
+  private def getGraph(entries: Map[(Int,Int), Int], filter: (((Int,Int),Option[Int]),((Int,Int),Int)) => Boolean): Map[(Int, Int), Map[(Int, Int), Int]] = {
     entries.map(coord => {
       val neighbours = List((0, 1), (1, 0), (-1, 0), (0, -1))
       val neighs = neighbours.map(pos => (coord._1._1 + pos._1, coord._1._2 + pos._2))
         .map(coord => (coord, entries.get(coord)))
         .filter(_._2.isDefined)
-        .filter(entry => (entry._2.get - entries(coord._1)) == 1 || (entry._2.get - entries(coord._1)) == 0 || (entry._2.get - entries(coord._1)) <= 0 )
+        .filter(entry => filter(entry,coord))
         .map(entry => (entry._1, 1))
         .toMap
       (coord._1, neighs)
@@ -33,9 +36,14 @@ object day12 extends App {
     def updatePath(entry: ((Int,Int), Int)): NodePaths = {
       val currentDistance = paths.getOrElse(entry._1, Integer.MAX_VALUE)
       val newDistance = entry._2 + paths.getOrElse(node, 0)
-      if (newDistance < currentDistance)
+      if (newDistance < currentDistance) {
+        val f = entries(entry._1)
+        if (f == 0 && !triggered) {
+          triggered = true
+          println(s"partb $newDistance")
+        }
         this.plus((entry._1, newDistance))
-      else
+      } else
         this
     }
 
@@ -60,23 +68,21 @@ object day12 extends App {
   private def getAlphabetIndex(column: (Char, Int)) = if (column._1 == 'S') -1 else if (column._1 == 'E') 26 else column._1.toInt - 97
 
   io.load("day12") { lines =>
-    implicit val entries: Map[(Int, Int), Int] = lines.zipWithIndex.flatMap(row => row._1.zipWithIndex.map(column => ((column._2, row._2), getAlphabetIndex(column)))).sorted.toMap
-    val e = getGraph(entries)
+    entries = lines.zipWithIndex.flatMap(row => row._1.zipWithIndex.map(column => ((column._2, row._2), getAlphabetIndex(column)))).sorted.toMap
+    val e1 = getGraph(entries, (entry, coord) => (entry._2.get - entries(coord._1)) == 1 || (entry._2.get - entries(coord._1)) <= 0)
+    val e2 = getGraph(entries, (entry, coord) => (entry._2.get - entries(coord._1)) == 1 || (entry._2.get - entries(coord._1)) >= -1)
     val end = entries.find(_._2 == 26).get
     val start = entries.find(_._2 == -1).get
 
-    val eff = findShortestPath(end._1,(0,0),e)
-
-    val firstPath = findShortestPath(start._1,end._1,e)
+    triggered = true
+    val firstPath = findShortestPath(start._1,end._1,e1)
     println(firstPath.paths)
     println(firstPath.paths.get(end._1))
 
-    val startPositions = entries.filter(entry => entry._2 == -1 || entry._2 == 0 ).toList
-    println(startPositions.size)
-    val test = startPositions.zipWithIndex.map(entry => {
-      println(s"${entry._2 + 1} from ${startPositions.size}")
-      (entry,findShortestPath(entry._1._1, end._1, e).paths.values.min)
-    })
-    println(test.map(_._2).min)
+    triggered = false
+    val secondPath = findShortestPath(end._1,start._1,e2)
+    println(secondPath.paths)
+    println(secondPath.paths.get(start._1))
+
   }
 }
