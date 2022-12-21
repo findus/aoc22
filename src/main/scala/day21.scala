4bf496ff85e60206afba31c662fb89375a2d96d5
Philipp <philipp@f1ndus.de>
2022-12-21 11:13:03 +0100

    day21a

--- src/main/scala/day21.scala
+++ src/main/scala/day21.scala
@@ -5,48 +5,36 @@ object  day21 extends App {
   val regex1 = "([a-z]{4}): ([a-z]{4}) ([+-/*]) ([a-z]{4})".r
   val regex2 = "([a-z]{4}): (\\d+)".r
 
-  def getValue: (String,String,String,String, mutable.Map[String, Long]) => Option[Long] = {
-    (name,x,op,y,map) => {
-      val mx = map.get(x)
-      val my = map.get(y)
-      if (mx.isEmpty && my.isEmpty)
-        Option.empty[Long]
-      else {
-        val value = op match {
-          case "*" => Some((mx.get * my.get).toLong)
-          case "-" => Some((mx.get - my.get).toLong)
-          case "+" => Some((mx.get + my.get).toLong)
-          case "/" => Some((mx.get / my.get).toLong)
+  def solve(monkeh: Monkeh, map: Map[String,Monkeh]): Long = {
+    monkeh match {
+      case m: NumberMonkeh => m.nr
+      case d: DependentMonkeh =>
+        val x1 = solve(map(d.x1), map)
+        val x2 = solve(map(d.x2), map)
+        d.operant match {
+          case "*" => x1 * x2
+          case "-" => x1 - x2
+          case "+" => x1 + x2
+          case "/" => x1 / x2
         }
-        map.put(name,value.get)
-        value
-      }
     }
   }
 
-  trait Monkeh
-  case class NumberMonkeh(name: String, nr: Long, map: mutable.Map[String, Long]) extends Monkeh
-  case class DependentMonkeh(name: String, get: () => Option[Long], map: mutable.Map[String, Long]) extends Monkeh
+  trait Monkeh {
+    def name: String
+  }
+  case class NumberMonkeh(name: String, nr: Long) extends Monkeh
+  case class DependentMonkeh(name: String, x1: String, x2: String, operant: String) extends Monkeh
 
   io.load("day21") { lines =>
-    val valueCache = collection.mutable.Map.empty[String,Long]
     val monkehs = lines.map {
-      case regex1(mname, x1, operant, x2) => DependentMonkeh(mname, () => getValue(mname,x1, operant, x2, valueCache), valueCache)
-      case regex2(mname, x1) => NumberMonkeh(mname, Integer.parseInt(x1), valueCache)
+      case regex1(mname, x1, operant, x2) => DependentMonkeh(mname, x1, x2, operant)
+      case regex2(mname, x1) => NumberMonkeh(mname, Integer.parseInt(x1))
     }
 
-    println(monkehs)
-    monkehs.map {
-      case monkeh1: NumberMonkeh =>
-        valueCache.put(monkeh1.name, monkeh1.nr)
-        monkeh1
-      case monkeh2 => monkeh2
-    }
-      .map {
-
-      }
-
-
+    val map = monkehs.map(m => (m.name, m)).toMap
+    val test = solve(monkehs.filter(_.name.equals("root")).head, map)
+    println(test)
   }
 
 }
