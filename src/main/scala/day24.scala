408cf9c8d4b30472933f6da40daa2bc6425eaa39
Philipp <philipp@f1ndus.de>
2022-12-24 15:08:38 +0100

    24b

--- src/main/scala/day24.scala
+++ src/main/scala/day24.scala
@@ -8,36 +8,15 @@ object day24 extends App {
   val blizzards = List('>','<','v','^')
   val orthogonal = Seq((1, 0), (-1, 0), (0, 1), (0, -1))
   val blizzardMovement = Map(
-    '>' -> (1 , 0),
-    '<' -> (-1, 0),
-    '^' -> (0 ,-1),
-    'v' -> (0 , 1)
+    '>' -> (1 , 0, 1),
+    '<' -> (-1, 0, 1),
+    '^' -> (0 ,-1, 1),
+    'v' -> (0 , 1, 1)
   )
 
-  case class Point(x: Int, y: Int) extends Ordering[Point] with Ordered[Point] {
-    override def compare(a: Point, b: Point): Int = if (a.y == b.y) a.x - b.x else b.y - a.y
-    override def compare(that: Point): Int = compare(this,that)
-    def adjacent: Seq[Point] = orthogonal.map(e => this.delta(e._1,e._2))
-    def delta(dx: Int, dy: Int) = Point(x + dx, y + dy)
-    def +(point: Point) = Point(x + point.x, y + point.y)
-  }
-
-  def bfs(grid: Set[Point], blizzards: List[Point], start: Point, end: Point): Int = {
-    val cost = collection.mutable.Map(start -> 0)
-    val todo = collection.mutable.Queue(start)
-
-    while (todo.nonEmpty) {
-      val current = todo.dequeue()
-      if (current == end) return cost(current)
-
-      val nextCost = cost(current) + 1
-      current.adjacent.filter(grid.contains).foreach { next =>
-        if (!cost.contains(next) || nextCost < cost(next))
-          cost(next) = nextCost
-        todo.enqueue(next)
-      }
-    }
-    -1
+  case class Point(x: Int, y: Int, z: Int) {
+    def delta(dx: Int, dy: Int, dz: Int) = Point(x + dx, y + dy, z + dz)
+    def +(point: Point) = Point(x + point.x, y + point.y, z + point.z)
   }
 
   val ordered = new Ordering[Point] {
@@ -47,7 +26,7 @@ object day24 extends App {
   io.load("day24") { lines =>
     val coords = SortedMap.from(lines.zipWithIndex.flatMap( { case(line,y) =>
       line.zipWithIndex.map { case (character,x) =>
-        (Point(x,y),character)
+        (Point(x,y,0),character)
       }
     }).toSet)(ordered)
 
@@ -57,52 +36,65 @@ object day24 extends App {
     val start = ground.minBy(_.y)
     val end = ground.maxBy(_.y)
 
-    val maxX = ground.maxBy(_.x)
-    val maxY = ground.maxBy(_.y)
+    val maxX = 100 // ground.maxBy(_.x)
+    val maxY = 35 //ground.maxBy(_.y)
 
-    def draw(grid: List[Point], blizzards: List[(Point,Char)]) = {
-      lines.zipWithIndex.foreach { case(line,y) =>
-        (line.zipWithIndex.foreach) { case(_,x) =>
-          if (walls.contains(Point(x,y))) print("#")
-          if (ground.contains(Point(x,y)) && !blizzards.map(_._1).contains(Point(x,y))) print(".")
-          if (blizzards.map(_._1).contains(Point(x,y))) print(blizzards.find(e => e._1.equals(Point(x,y))).get._2)
+    def bfs(start: Point, end: Point, points:Set[Point]): Int = {
+      val todo = collection.mutable.Queue(start)
+      val cost = collection.mutable.Map(start -> 0)
+
+      while (todo.nonEmpty) {
+        val cur = todo.dequeue()
+
+        if (cur.x == end.x && cur.y == end.y) return cost(cur)
+
+        val neighbours = Seq(
+          Point(cur.x, cur.y, cur.z + 1),
+          Point(cur.x - 1, cur.y, cur.z + 1),
+          Point(cur.x + 1, cur.y, cur.z + 1),
+          Point(cur.x, cur.y - 1, cur.z + 1),
+          Point(cur.x, cur.y + 1, cur.z + 1),
+        )
+
+        neighbours.filterNot(cost.contains).filterNot(points.contains).foreach { next =>
+          todo += next
+          cost(next) = cost(cur) + 1
         }
-        println("")
       }
-      println("")
+      -1
     }
 
-    def newBlizzardPos(point: Point, direction: Char) = {
-      val (x,y) = blizzardMovement(direction)
-      val newPoint = point + Point(x,y)
-      val newBlizzardPosition = if (walls.contains(newPoint)) {
-        direction match {
-          case '>' => Point(1         , newPoint.y)
-          case '<' => Point(maxX.x    , newPoint.y)
-          case '^' => Point(newPoint.x, maxY.y    )
-          case 'v' => Point(newPoint.x, 1         )
-        }
-      } else newPoint
-      newBlizzardPosition
+    val points = collection.mutable.Set[Point]()
+    var (right, down, left, up, wall) = (blizzardPos.filter(_._2=='>').map(_._1),blizzardPos.filter(_._2=='v').map(_._1),blizzardPos.filter(_._2=='<').map(_._1),blizzardPos.filter(_._2=='^').map(_._1),walls.toList)
+    wall = wall.appended(Point(1, -1, 0))
+    points ++= right ++= down ++= left ++= down ++= wall
+
+    for (_ <- 1 to 1000){
+      right = right.map { cur =>
+        if (cur.x == maxX) Point(1, cur.y, cur.z + 1) else Point(cur.x + 1, cur.y, cur.z + 1)
+      }
+      left = left.map { cur =>
+        // maybe -1
+        if (cur.x == 1) Point(maxX, cur.y, cur.z + 1) else Point(cur.x - 1, cur.y, cur.z + 1)
+      }
+      up = up.map { cur =>
+        if (cur.y == 1)  Point(cur.x, maxY, cur.z + 1) else Point(cur.x, cur.y - 1, cur.z + 1)
+      }
+      down = down.map { cur =>
+        if (cur.y == maxY) Point(cur.x, 1, cur.z + 1) else Point(cur.x, cur.y + 1, cur.z + 1)
+      }
+      wall = wall.map { cur =>
+        Point(cur.x, cur.y, cur.z + 1)
+      }
+      points ++= right ++= down ++= left ++= up ++= wall
     }
 
-    case class State(position: Point, blizzards:  List[(Point,Char)], walls: Set[Point], steps: Int)
+    val result = bfs(start,end,points.toSet)
+    println(result)
+    val result2 = bfs(end.copy(z=result),start,points.toSet)
+    val result3 = bfs(start.copy(z=result2+result),end,points.toSet)
+    println(result+result2+result3)
 
-    def simulate(state: State): State = {
-
-      val newBlizzards = state.blizzards.map { case(p,d) => (newBlizzardPos(p,d),d) }
-
-//      val next = state.position.adjacent.filter(entry => !walls.contains(entry) && !newBlizzards.map(_._1).contains(entry) && entry.y > 0 )
-//      draw(coords.keys.toList, newBlizzards)
-//      val s = next.map(next => simulate(State(next, newBlizzards, walls, state.steps + 1)))
-//      s.minBy(_.steps)
-    }
-
-    val result = simulate(State(start, blizzardPos, walls, 0))
-
-
-
-    println(coords)
   }
 
 }
